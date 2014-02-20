ea475daff911a661367b3478c0314724b13fae83
jared abongo <abongoje@gmail.com>
2014-02-20 14:06:52 +0300

    add navbar

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+  alert ("Hello Js!!!");
+  var message;
+  if (message==="Undefined"){
+  	alert("True!!" + message)
+  }
+  else
+  {
+  	alert("False!!" + message)
+  }
+  var button = document.getElementById('My-button')
+  console.log("button is:" + button)
+  button.addEventListener('click',function(e))
+  {
+  	alert("button was clicked!!")
+  }
+ $("#My-button")on ("click");function(){alert ("you clicked!!!")})
